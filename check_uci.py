f4c75cb91155d6ccd064417fffecc986dbaf5240
Amber Yasin <writetoamberyasin@gmail.com>
2026-04-27 14:16:42 +0100

    cleanup — remove temp dagster files and helper scripts

--- check_uci.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-client = MongoClient('mongodb://localhost:27017/')
-db = client['ecommerce_db']
-doc = db['uci_raw'].find_one()
-print('uci_raw columns:')
-print(list(doc.keys()))
